c1a1420791e5dbb200d36ce4bd8c06c0fed80774
Florent Kermarrec <florent@enjoy-digital.fr>
2017-03-07 11:11:40 +0100

    merge targets in single files

--- kc705.py
+++ kc705.py
@@ -1,15 +1,133 @@
 #!/usr/bin/env python3
 
-import argparse
-
 from litex.gen import *
 from litex.build.generic_platform import *
 
+from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
-from litex.soc.interconnect.csr import *
+from litex.soc.cores.uart.bridge import UARTWishboneBridge
 
-from nexys_platform import Platform
-from nexys_base import BaseSoC
+from liteeth.common import convert_ip
+from liteeth.phy import LiteEthPHY
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
+
+from litex.boards.platforms import kc705
+
+
+_usb3_io = [
+    # HiTechGlobal USB3.0 FMC P1 connector
+    ("usb_clkout", 0, Pins("HPC:LA00_CC_P"), IOStandard("LVCMOS25")),
+    ("usb_reset_n", 0, Pins("HPC:LA32_N"), IOStandard("LVCMOS25")),
+    ("usb_ulpi", 0,
+        Subsignal("clk", Pins("HPC:HA01_CC_P")),
+        Subsignal("data", Pins(
+            "HPC:HA11_N", "HPC:HA13_N", "HPC:HA15_N", "HPC:HA12_N",
+            "HPC:HA14_N", "HPC:HA15_P", "HPC:HA16_P", "HPC:HA12_P")),
+        Subsignal("dir", Pins("HPC:HA14_P")),
+        Subsignal("stp", Pins("HPC:HA16_N")),
+        Subsignal("nxt", Pins("HPC:HA13_P")),
+        IOStandard("LVCMOS25"),
+    ),
+    ("usb_pipe_ctrl", 0,
+        Subsignal("phy_reset_n", Pins("HPC:LA12_P")),
+        Subsignal("tx_detrx_lpbk", Pins("HPC:LA29_N")),
+        Subsignal("tx_elecidle", Pins("HPC:LA27_P")),
+        Subsignal("power_down", Pins("HPC:LA12_N", "HPC:LA13_P")),
+        Subsignal("tx_oneszeros", Pins("HPC:LA27_N")),
+        Subsignal("tx_deemph", Pins("HPC:LA31_P", "HPC:LA28_N")),
+        Subsignal("tx_margin", Pins("HPC:LA30_P", "HPC:LA30_N")),
+        Subsignal("tx_swing", Pins("HPC:LA29_P")),
+        Subsignal("rx_polarity", Pins("HPC:LA16_N")),
+        Subsignal("rx_termination", Pins("HPC:LA13_N")),
+        Subsignal("rate", Pins("HPC:LA28_P")),
+        Subsignal("elas_buf_mode", Pins("HPC:LA15_N")),
+        IOStandard("LVCMOS25"),
+    ),
+    ("usb_pipe_status", 0,
+        Subsignal("rx_elecidle", Pins("HPC:LA11_N")),
+        Subsignal("rx_status", Pins("HPC:LA14_P", "HPC:LA15_P", "HPC:LA14_N")),
+        Subsignal("phy_status", Pins("HPC:LA16_P")),
+        Subsignal("pwr_present", Pins("HPC:LA32_P")),
+        IOStandard("LVCMOS25"),
+    ),
+    ("usb_pipe_data", 0,
+        Subsignal("rx_clk", Pins("HPC:LA01_CC_P")),
+        Subsignal("rx_valid", Pins("HPC:LA11_P")),
+        Subsignal("rx_data", Pins(
+            "HPC:LA10_N", "HPC:LA10_P", "HPC:LA09_N", "HPC:LA09_P",
+            "HPC:LA07_N", "HPC:LA08_N", "HPC:LA05_N", "HPC:LA03_N",
+            "HPC:LA06_N", "HPC:LA02_P", "HPC:LA06_P", "HPC:LA04_P",
+            "HPC:LA03_P", "HPC:LA08_P", "HPC:LA07_P", "HPC:LA04_N")),
+        Subsignal("rx_datak", Pins("HPC:LA02_N", "HPC:LA05_P")),
+
+        Subsignal("tx_clk", Pins("HPC:LA20_P")),
+        Subsignal("tx_data", Pins(
+            "HPC:LA24_N", "HPC:LA26_N", "HPC:LA24_P", "HPC:LA26_P",
+            "HPC:LA25_N", "HPC:LA21_N", "HPC:LA25_P", "HPC:LA21_P",
+            "HPC:LA22_P", "HPC:LA22_N", "HPC:LA19_N", "HPC:LA23_N",
+            "HPC:LA23_P", "HPC:LA18_CC_P", "HPC:LA19_P", "HPC:LA20_N")),
+        Subsignal("tx_datak", Pins("HPC:LA18_CC_N", "HPC:LA17_CC_P")),
+        IOStandard("LVCMOS25"),
+    ),
+]
+
+
+class Platform(kc705.Platform):
+    def __init__(self, *args, **kwargs):
+        kc705.Platform.__init__(self, *args, **kwargs)
+        self.add_extension(_usb3_io)
+
+
+class BaseSoC(SoCCore):
+    csr_map = {
+        "eth_phy":  11,
+        "eth_core": 12
+    }
+    csr_map.update(SoCCore.csr_map)
+    def __init__(self, platform,
+        mac_address=0x10e2d5000000,
+        ip_address="192.168.1.50"):
+        clk_freq = int(1e9/platform.default_clk_period)
+        SoCCore.__init__(self, platform, clk_freq,
+            cpu_type=None,
+            csr_data_width=32,
+            with_uart=False,
+            ident="Daisho USB3.0 Test Design",
+            with_timer=False
+        )
+        self.submodules.crg = CRG(platform.request(platform.default_clk_name))
+
+        # uart <--> wishbone
+        self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"),
+                                                  clk_freq, baudrate=115200))
+        self.add_wb_master(self.cpu_or_bridge.wishbone)
+
+        # ethernet PHY and UDP/IP stack
+        self.submodules.eth_phy = LiteEthPHY(platform.request("eth_clocks"),
+                                             platform.request("eth"),
+                                             clk_freq=clk_freq)
+        self.submodules.eth_core = LiteEthUDPIPCore(self.eth_phy,
+                                                    mac_address,
+                                                    convert_ip(ip_address),
+                                                    clk_freq)
+
+        # ethernet <--> wishbone
+        self.submodules.etherbone = LiteEthEtherbone(self.eth_core.udp, 1234)
+        self.add_wb_master(self.etherbone.master.bus)
+
+        # timing constraints
+        self.crg.cd_sys.clk.attr.add("keep")
+        self.eth_phy.crg.cd_eth_rx.clk.attr.add("keep")
+        self.eth_phy.crg.cd_eth_tx.clk.attr.add("keep")
+        self.platform.add_period_constraint(self.crg.cd_sys.clk, 6.0)
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_rx.clk, 8.0)
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_tx.clk, 8.0)
+
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.eth_phy.crg.cd_eth_rx.clk,
+            self.eth_phy.crg.cd_eth_tx.clk)
 
 
 class USBSoC(BaseSoC):
@@ -19,6 +137,7 @@ class USBSoC(BaseSoC):
         # usb ios
         usb_clkout = platform.request("usb_clkout")
         usb_reset_n = platform.request("usb_reset_n")
+        usb_ulpi = platform.request("usb_ulpi")
         usb_pipe_ctrl = platform.request("usb_pipe_ctrl")
         usb_pipe_status = platform.request("usb_pipe_status")
         usb_pipe_data = platform.request("usb_pipe_data")
@@ -84,8 +203,55 @@ class USBSoC(BaseSoC):
         ]
 
         # usb2 core
-        # ulpi interface use HA FMC signals only available on HPC
-        # so no usb2 core on this platform
+        self.specials += Instance("usb2_top",
+            i_ext_clk=usb_clkout, # TODO
+            i_reset_n=1, # TODO
+            #o_reset_n_out=,
+
+            i_phy_ulpi_clk=usb_ulpi.clk,
+            io_phy_ulpi_d=usb_ulpi.data,
+            i_phy_ulpi_dir=usb_ulpi.dir,
+            o_phy_ulpi_stp=usb_ulpi.stp,
+            i_phy_ulpi_nxt=usb_ulpi.nxt,
+
+            i_opt_disable_all=0, # TODO
+            i_opt_enable_hs=0, # TODO
+            i_opt_ignore_vbus=0, # TODO
+            #o_stat_connected=, # TODO
+            #o_stat_fs=, # TODO
+            #o_stat_hs=, # TODO
+            #o_stat_configured=, # TODO
+
+            i_buf_in_addr=0, # TODO
+            i_buf_in_data=0, # TODO
+            i_buf_in_wren=0, # TODO
+            #o_buf_in_ready=, # TODO
+            i_buf_in_commit=0, # TODO
+            i_buf_in_commit_len=0, # TODO
+            #o_buf_in_commit_ack=, # TODO
+            i_buf_out_addr=0, # TODO
+            #o_buf_out_q=, # TODO
+            #o_buf_out_len=, # TODO
+            #o_buf_out_hasdata=, # TODO
+            i_buf_out_arm=0, # TODO
+            #o_buf_out_arm_ack=, # TODO
+
+            #o_vend_req_act=, # TODO
+            #o_vend_req_request=, # TODO
+            #o_vend_req_val=, # TODO
+
+            #o_err_crc_pid=, # TODO
+            #o_err_crc_tok=, # TODO
+            #o_err_crc_pkt=, # TODO
+            #o_err_pid_out_of_seq=, # TODO
+            #o_err_setup_pkt=, # TODO
+
+            #o_dbg_frame_num=, # TODO
+            #o_dbg_linestate= # TODO
+        )
+        platform.add_verilog_include_path(os.path.join("core"))
+        platform.add_verilog_include_path(os.path.join("core", "usb2"))
+        platform.add_source_dir(os.path.join("core", "usb2"))
 
         # usb3 core
 
@@ -189,6 +355,7 @@ class USBSoC(BaseSoC):
                 i_C=ClockSignal(), i_CE=1, i_S=0, i_R=0,
                 i_D1=phy_pipe_tx_datak[i], i_D2=phy_pipe_tx_datak[2+i], o_Q=usb_pipe_data.tx_datak[i],
             )
+
 
 def main():
     platform = Platform(toolchain="vivado")
--- nexys_video.py
+++ nexys_video.py
@@ -1,17 +1,132 @@
 #!/usr/bin/env python3
 
-import argparse
-
 from litex.gen import *
 from litex.build.generic_platform import *
 
+from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
-from litex.soc.interconnect.csr import *
+from litex.soc.cores.uart.bridge import UARTWishboneBridge
 
-from litex.gen.genlib.io import DifferentialInput, DifferentialOutput
+from liteeth.common import convert_ip
+from liteeth.phy.s7rgmii import LiteEthPHYRGMII
+from liteeth.core import LiteEthUDPIPCore
+from liteeth.frontend.etherbone import LiteEthEtherbone
 
-from kc705_platform import Platform
-from kc705_base import BaseSoC
+from litex.boards.platforms import nexys_video
+
+
+_usb3_io = [
+    # HiTechGlobal USB3.0 FMC P1 connector
+    ("usb_clkout", 0, Pins("LPC:LA00_CC_P"), IOStandard("LVCMOS25")),
+    ("usb_reset_n", 0, Pins("LPC:LA32_N"), IOStandard("LVCMOS25")),
+    ("usb_ulpi", 0,
+        Subsignal("clk", Pins("LPC:HA01_CC_P")),
+        Subsignal("data", Pins(
+            "LPC:HA11_N", "LPC:HA13_N", "LPC:HA15_N", "LPC:HA12_N",
+            "LPC:HA14_N", "LPC:HA15_P", "LPC:HA16_P", "LPC:HA12_P")),
+        Subsignal("dir", Pins("LPC:HA14_P")),
+        Subsignal("stp", Pins("LPC:HA16_N")),
+        Subsignal("nxt", Pins("LPC:HA13_P")),
+        IOStandard("LVCMOS25"),
+    ),
+    ("usb_pipe_ctrl", 0,
+        Subsignal("phy_reset_n", Pins("LPC:LA12_P")),
+        Subsignal("tx_detrx_lpbk", Pins("LPC:LA29_N")),
+        Subsignal("tx_elecidle", Pins("LPC:LA27_P")),
+        Subsignal("power_down", Pins("LPC:LA12_N", "LPC:LA13_P")),
+        Subsignal("tx_oneszeros", Pins("LPC:LA27_N")),
+        Subsignal("tx_deemph", Pins("LPC:LA31_P", "LPC:LA28_N")),
+        Subsignal("tx_margin", Pins("LPC:LA30_P", "LPC:LA30_N")),
+        Subsignal("tx_swing", Pins("LPC:LA29_P")),
+        Subsignal("rx_polarity", Pins("LPC:LA16_N")),
+        Subsignal("rx_termination", Pins("LPC:LA13_N")),
+        Subsignal("rate", Pins("LPC:LA28_P")),
+        Subsignal("elas_buf_mode", Pins("LPC:LA15_N")),
+        IOStandard("LVCMOS25"),
+    ),
+    ("usb_pipe_status", 0,
+        Subsignal("rx_elecidle", Pins("LPC:LA11_N")),
+        Subsignal("rx_status", Pins("LPC:LA14_P", "LPC:LA15_P", "LPC:LA14_N")),
+        Subsignal("phy_status", Pins("LPC:LA16_P")),
+        Subsignal("pwr_present", Pins("LPC:LA32_P")),
+        IOStandard("LVCMOS25"),
+    ),
+    ("usb_pipe_data", 0,
+        Subsignal("rx_clk", Pins("LPC:LA01_CC_P")),
+        Subsignal("rx_valid", Pins("LPC:LA11_P")),
+        Subsignal("rx_data", Pins(
+            "LPC:LA10_N", "LPC:LA10_P", "LPC:LA09_N", "LPC:LA09_P",
+            "LPC:LA07_N", "LPC:LA08_N", "LPC:LA05_N", "LPC:LA03_N",
+            "LPC:LA06_N", "LPC:LA02_P", "LPC:LA06_P", "LPC:LA04_P",
+            "LPC:LA03_P", "LPC:LA08_P", "LPC:LA07_P", "LPC:LA04_N")),
+        Subsignal("rx_datak", Pins("LPC:LA02_N", "LPC:LA05_P")),
+
+        Subsignal("tx_clk", Pins("LPC:LA20_P")),
+        Subsignal("tx_data", Pins(
+            "LPC:LA24_N", "LPC:LA26_N", "LPC:LA24_P", "LPC:LA26_P",
+            "LPC:LA25_N", "LPC:LA21_N", "LPC:LA25_P", "LPC:LA21_P",
+            "LPC:LA22_P", "LPC:LA22_N", "LPC:LA19_N", "LPC:LA23_N",
+            "LPC:LA23_P", "LPC:LA18_CC_P", "LPC:LA19_P", "LPC:LA20_N")),
+        Subsignal("tx_datak", Pins("LPC:LA18_CC_N", "LPC:LA17_CC_P")),
+        IOStandard("LVCMOS25"),
+    ),
+]
+
+
+class Platform(nexys_video.Platform):
+    def __init__(self, *args, **kwargs):
+        nexys_video.Platform.__init__(self, *args, **kwargs)
+        self.add_extension(_usb3_io)
+
+
+class BaseSoC(SoCCore):
+    csr_map = {
+        "eth_phy":  11,
+        "eth_core": 12
+    }
+    csr_map.update(SoCCore.csr_map)
+    def __init__(self, platform,
+        mac_address=0x10e2d5000000,
+        ip_address="192.168.1.50"):
+        clk_freq = int(1e9/platform.default_clk_period)
+        SoCCore.__init__(self, platform, clk_freq,
+            cpu_type=None,
+            csr_data_width=32,
+            with_uart=False,
+            ident="Daisho USB3.0 Test Design",
+            with_timer=False
+        )
+        self.submodules.crg = CRG(platform.request(platform.default_clk_name))
+
+        # uart <--> wishbone
+        self.add_cpu_or_bridge(UARTWishboneBridge(platform.request("serial"),
+                                                  clk_freq, baudrate=115200))
+        self.add_wb_master(self.cpu_or_bridge.wishbone)
+
+        # ethernet PHY and UDP/IP stack
+        self.submodules.eth_phy = LiteEthPHYRGMII(self.platform.request("eth_clocks"),
+                                                 self.platform.request("eth"))
+        self.submodules.eth_core = LiteEthUDPIPCore(self.eth_phy,
+                                                    mac_address,
+                                                    convert_ip(ip_address),
+                                                    clk_freq)
+
+        # ethernet <--> wishbone
+        self.submodules.etherbone = LiteEthEtherbone(self.eth_core.udp, 1234)
+        self.add_wb_master(self.etherbone.master.bus)
+
+        # timing constraints
+        self.crg.cd_sys.clk.attr.add("keep")
+        self.eth_phy.crg.cd_eth_rx.clk.attr.add("keep")
+        self.eth_phy.crg.cd_eth_tx.clk.attr.add("keep")
+        self.platform.add_period_constraint(self.crg.cd_sys.clk, 6.0)
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_rx.clk, 8.0)
+        self.platform.add_period_constraint(self.eth_phy.crg.cd_eth_tx.clk, 8.0)
+
+        self.platform.add_false_path_constraints(
+            self.crg.cd_sys.clk,
+            self.eth_phy.crg.cd_eth_rx.clk,
+            self.eth_phy.crg.cd_eth_tx.clk)
 
 
 class USBSoC(BaseSoC):
@@ -21,7 +136,6 @@ class USBSoC(BaseSoC):
         # usb ios
         usb_clkout = platform.request("usb_clkout")
         usb_reset_n = platform.request("usb_reset_n")
-        usb_ulpi = platform.request("usb_ulpi")
         usb_pipe_ctrl = platform.request("usb_pipe_ctrl")
         usb_pipe_status = platform.request("usb_pipe_status")
         usb_pipe_data = platform.request("usb_pipe_data")
@@ -87,55 +201,8 @@ class USBSoC(BaseSoC):
         ]
 
         # usb2 core
-        self.specials += Instance("usb2_top",
-            i_ext_clk=usb_clkout, # TODO
-            i_reset_n=1, # TODO
-            #o_reset_n_out=,
-
-            i_phy_ulpi_clk=usb_ulpi.clk,
-            io_phy_ulpi_d=usb_ulpi.data,
-            i_phy_ulpi_dir=usb_ulpi.dir,
-            o_phy_ulpi_stp=usb_ulpi.stp,
-            i_phy_ulpi_nxt=usb_ulpi.nxt,
-
-            i_opt_disable_all=0, # TODO
-            i_opt_enable_hs=0, # TODO
-            i_opt_ignore_vbus=0, # TODO
-            #o_stat_connected=, # TODO
-            #o_stat_fs=, # TODO
-            #o_stat_hs=, # TODO
-            #o_stat_configured=, # TODO
-
-            i_buf_in_addr=0, # TODO
-            i_buf_in_data=0, # TODO
-            i_buf_in_wren=0, # TODO
-            #o_buf_in_ready=, # TODO
-            i_buf_in_commit=0, # TODO
-            i_buf_in_commit_len=0, # TODO
-            #o_buf_in_commit_ack=, # TODO
-            i_buf_out_addr=0, # TODO
-            #o_buf_out_q=, # TODO
-            #o_buf_out_len=, # TODO
-            #o_buf_out_hasdata=, # TODO
-            i_buf_out_arm=0, # TODO
-            #o_buf_out_arm_ack=, # TODO
-
-            #o_vend_req_act=, # TODO
-            #o_vend_req_request=, # TODO
-            #o_vend_req_val=, # TODO
-
-            #o_err_crc_pid=, # TODO
-            #o_err_crc_tok=, # TODO
-            #o_err_crc_pkt=, # TODO
-            #o_err_pid_out_of_seq=, # TODO
-            #o_err_setup_pkt=, # TODO
-
-            #o_dbg_frame_num=, # TODO
-            #o_dbg_linestate= # TODO
-        )
-        platform.add_verilog_include_path(os.path.join("core"))
-        platform.add_verilog_include_path(os.path.join("core", "usb2"))
-        platform.add_source_dir(os.path.join("core", "usb2"))
+        # ulpi interface use HA FMC signals only available on HPC
+        # so no usb2 core on this platform
 
         # usb3 core
 
@@ -239,6 +306,7 @@ class USBSoC(BaseSoC):
                 i_C=ClockSignal(), i_CE=1, i_S=0, i_R=0,
                 i_D1=phy_pipe_tx_datak[i], i_D2=phy_pipe_tx_datak[2+i], o_Q=usb_pipe_data.tx_datak[i],
             )
+
 
 def main():
     platform = Platform(toolchain="vivado")
