cec9c6f621ba4479a298a7e7be9f20b1b857995a
Florent Kermarrec <florent@enjoy-digital.fr>
2017-04-04 09:30:04 +0200

    usb2/usb3: expose buf_in/buf_out to csrs (to prevent synthesis optimizations for ressources estimation)

--- kc705.py
+++ kc705.py
@@ -167,10 +167,41 @@ class USBSoC(BaseSoC):
                     self._phy_enable = CSRStorage()
                     self._core_enable = CSRStorage()
 
+                    # probably not working but prevent synthesis optimizations
+                    self._buf_in_addr = CSRStorage(9)
+                    self._buf_in_data = CSRStorage(8)
+                    self._buf_in_wren = CSR()
+                    self._buf_in_ready = CSRStatus()
+                    self._buf_in_commit = CSR()
+                    self._buf_in_commit_len = CSRStorage(10)
+                    self._buf_in_commit_ack = CSRStatus()
+
+                    self._buf_out_addr = CSRStorage(9)
+                    self._buf_out_q = CSRStatus(8)
+                    self._buf_out_len = CSRStatus(10)
+                    self._buf_out_hasdata = CSRStatus()
+                    self._buf_out_arm = CSR()
+                    self._buf_out_arm_ack = CSRStatus()
+
                     # # #
 
                     self.phy_enable = self._phy_enable.storage
                     self.core_enable = self._core_enable.storage
+
+                    self.buf_in_addr = self._buf_in_addr.storage
+                    self.buf_in_data = self._buf_in_data.storage
+                    self.buf_in_wren = self._buf_in_wren.re & self._buf_in_wren.r
+                    self.buf_in_ready = self._buf_in_ready.status
+                    self.buf_in_commit = self._buf_in_commit.re & self._buf_in_commit.r
+                    self.buf_in_commit_len = self._buf_in_commit_len.storage
+                    self.buf_in_commit_ack = self._buf_in_commit_ack.status
+
+                    self.buf_out_addr = self._buf_out_addr.storage
+                    self.buf_out_q = self._buf_out_q.status
+                    self.buf_out_len = self._buf_out_len.status
+                    self.buf_out_hasdata = self._buf_out_hasdata.status
+                    self.buf_out_arm = self._buf_out_arm.re & self._buf_out_arm.r
+                    self.buf_out_arm_ack = self._buf_out_arm_ack.status
 
 
             self.submodules.usb2_control = USB2Control()
@@ -223,20 +254,20 @@ class USBSoC(BaseSoC):
                 o_stat_hs=stat_hs,
                 o_stat_configured=stat_configured,
 
-                i_buf_in_addr=0, # TODO
-                i_buf_in_data=0, # TODO
-                i_buf_in_wren=0, # TODO
-                #o_buf_in_ready=, # TODO
-                i_buf_in_commit=0, # TODO
-                i_buf_in_commit_len=0, # TODO
-                #o_buf_in_commit_ack=, # TODO
+                i_buf_in_addr=self.usb2_control.buf_in_addr,
+                i_buf_in_data=self.usb2_control.buf_in_data,
+                i_buf_in_wren=self.usb2_control.buf_in_wren,
+                o_buf_in_ready=self.usb2_control.buf_in_ready,
+                i_buf_in_commit=self.usb2_control.buf_in_commit,
+                i_buf_in_commit_len=self.usb2_control.buf_in_commit_len,
+                o_buf_in_commit_ack=self.usb2_control.buf_in_commit_ack,
 
-                i_buf_out_addr=0, # TODO
-                #o_buf_out_q=, # TODO
-                #o_buf_out_len=, # TODO
-                #o_buf_out_hasdata=, # TODO
-                i_buf_out_arm=0, # TODO
-                #o_buf_out_arm_ack=, # TODO
+                i_buf_out_addr=self.usb2_control.buf_out_addr,
+                o_buf_out_q=self.usb2_control.buf_out_q,
+                o_buf_out_len=self.usb2_control.buf_out_len,
+                o_buf_out_hasdata=self.usb2_control.buf_out_hasdata,
+                i_buf_out_arm=self.usb2_control.buf_out_arm,
+                o_buf_out_arm_ack=self.usb2_control.buf_out_arm_ack,
 
                 o_vend_req_act=vend_req_act,
                 o_vend_req_request=vend_req_request,
@@ -287,10 +318,43 @@ class USBSoC(BaseSoC):
                     self._phy_enable = CSRStorage()
                     self._core_enable = CSRStorage()
 
+                    # probably not working but prevent synthesis optimizations
+                    self._buf_in_addr = CSRStorage(9)
+                    self._buf_in_data = CSRStorage(32)
+                    self._buf_in_wren = CSR()
+                    self._buf_in_request = CSRStatus()
+                    self._buf_in_ready = CSRStatus()
+                    self._buf_in_commit = CSR()
+                    self._buf_in_commit_len = CSRStorage(11)
+                    self._buf_in_commit_ack = CSRStatus()
+
+                    self._buf_out_addr = CSRStorage(9)
+                    self._buf_out_q = CSRStatus(32)
+                    self._buf_out_len = CSRStatus(11)
+                    self._buf_out_hasdata = CSRStatus()
+                    self._buf_out_arm = CSR()
+                    self._buf_out_arm_ack = CSRStatus()
+
                     # # #
 
                     self.phy_enable = self._phy_enable.storage
                     self.core_enable = self._core_enable.storage
+
+                    self.buf_in_addr = self._buf_in_addr.storage
+                    self.buf_in_data = self._buf_in_data.storage
+                    self.buf_in_wren = self._buf_in_wren.re & self._buf_in_wren.r
+                    self.buf_in_request = self._buf_in_request.status
+                    self.buf_in_ready = self._buf_in_ready.status
+                    self.buf_in_commit = self._buf_in_commit.re & self._buf_in_commit.r
+                    self.buf_in_commit_len = self._buf_in_commit_len.storage
+                    self.buf_in_commit_ack = self._buf_in_commit_ack.status
+
+                    self.buf_out_addr = self._buf_out_addr.storage
+                    self.buf_out_q = self._buf_out_q.status
+                    self.buf_out_len = self._buf_out_len.status
+                    self.buf_out_hasdata = self._buf_out_hasdata.status
+                    self.buf_out_arm = self._buf_out_arm.re & self._buf_out_arm.r
+                    self.buf_out_arm_ack = self._buf_out_arm_ack.status
 
 
             self.submodules.usb3_control = USB3Control()
@@ -424,25 +488,25 @@ class USBSoC(BaseSoC):
                 o_phy_rate=usb_pipe_ctrl.rate,
                 o_phy_elas_buf_mode=usb_pipe_ctrl.elas_buf_mode,
 
-                i_buf_in_addr=0, # TODO
-                i_buf_in_data=0, # TODO
-                i_buf_in_wren=0, # TODO
-                #o_buf_in_request=, # TODO
-                #o_buf_in_ready=, # TODO
-                i_buf_in_commit=0, # TODO
-                i_buf_in_commit_len=0, # TODO
-                #o_buf_in_commit_ack=, # TODO
+                i_buf_in_addr=self.usb3_control.buf_in_addr,
+                i_buf_in_data=self.usb3_control.buf_in_data,
+                i_buf_in_wren=self.usb3_control.buf_in_wren,
+                o_buf_in_request=self.usb3_control.buf_in_request,
+                o_buf_in_ready=self.usb3_control.buf_in_ready,
+                i_buf_in_commit=self.usb3_control.buf_in_commit,
+                i_buf_in_commit_len=self.usb3_control.buf_in_commit_len,
+                o_buf_in_commit_ack=self.usb3_control.buf_in_commit_ack,
 
-                i_buf_out_addr=0, # TODO
-                #o_buf_out_q=, # TODO
-                #o_buf_out_len=, # TODO
-                #o_buf_out_hasdata=, # TODO
-                i_buf_out_arm=0, # TODO
-                #o_buf_out_arm_ack=, # TODO
+                i_buf_out_addr=self.usb3_control.buf_out_addr,
+                o_buf_out_q=self.usb3_control.buf_out_q,
+                o_buf_out_len=self.usb3_control.buf_out_len,
+                o_buf_out_hasdata=self.usb3_control.buf_out_hasdata,
+                i_buf_out_arm=self.usb3_control.buf_out_arm,
+                o_buf_out_arm_ack=self.usb3_control.buf_out_arm_ack,
 
-                #o_vend_req_act=, # TODO
-                #o_vend_req_request=, # TODO
-                #o_vend_req_val=, # TODO
+                #o_vend_req_act=,
+                #o_vend_req_request=,
+                #o_vend_req_val=,
 
                 o_dbg_pipe_state=dbg_pipe_state,
                 o_dbg_ltssm_state=dbg_ltssm_state
--- nexys_video.py
+++ nexys_video.py
@@ -214,10 +214,43 @@ class USBSoC(BaseSoC):
                     self._phy_enable = CSRStorage()
                     self._core_enable = CSRStorage()
 
+                    # probably not working but prevent synthesis optimizations
+                    self._buf_in_addr = CSRStorage(9)
+                    self._buf_in_data = CSRStorage(32)
+                    self._buf_in_wren = CSR()
+                    self._buf_in_request = CSRStatus()
+                    self._buf_in_ready = CSRStatus()
+                    self._buf_in_commit = CSR()
+                    self._buf_in_commit_len = CSRStorage(11)
+                    self._buf_in_commit_ack = CSRStatus()
+
+                    self._buf_out_addr = CSRStorage(9)
+                    self._buf_out_q = CSRStatus(32)
+                    self._buf_out_len = CSRStatus(11)
+                    self._buf_out_hasdata = CSRStatus()
+                    self._buf_out_arm = CSR()
+                    self._buf_out_arm_ack = CSRStatus()
+
                     # # #
 
                     self.phy_enable = self._phy_enable.storage
                     self.core_enable = self._core_enable.storage
+
+                    self.buf_in_addr = self._buf_in_addr.storage
+                    self.buf_in_data = self._buf_in_data.storage
+                    self.buf_in_wren = self._buf_in_wren.re & self._buf_in_wren.r
+                    self.buf_in_request = self._buf_in_request.status
+                    self.buf_in_ready = self._buf_in_ready.status
+                    self.buf_in_commit = self._buf_in_commit.re & self._buf_in_commit.r
+                    self.buf_in_commit_len = self._buf_in_commit_len.storage
+                    self.buf_in_commit_ack = self._buf_in_commit_ack.status
+
+                    self.buf_out_addr = self._buf_out_addr.storage
+                    self.buf_out_q = self._buf_out_q.status
+                    self.buf_out_len = self._buf_out_len.status
+                    self.buf_out_hasdata = self._buf_out_hasdata.status
+                    self.buf_out_arm = self._buf_out_arm.re & self._buf_out_arm.r
+                    self.buf_out_arm_ack = self._buf_out_arm_ack.status
 
 
             self.submodules.usb3_control = USB3Control()
@@ -234,7 +267,6 @@ class USBSoC(BaseSoC):
             phy_pipe_half_clk_phase = Signal()
             phy_pipe_quarter_clk = Signal()
             phy_pipe_tx_clk_phase = Signal()
-
 
             self.specials += [
                 Instance("PLLE2_BASE",
@@ -266,7 +298,6 @@ class USBSoC(BaseSoC):
                 Instance("BUFG", i_I=phy_pipe_half_clk_phase_pll, o_O=phy_pipe_half_clk_phase),
                 Instance("BUFG", i_I=phy_pipe_quarter_clk_pll, o_O=phy_pipe_quarter_clk),
                 Instance("BUFG", i_I=phy_pipe_tx_clk_phase_pll, o_O=phy_pipe_tx_clk_phase),
-                #Instance("IDELAYCTRL", i_REFCLK=ClockSignal(), i_RST=ResetSignal()) # not used
             ]
 
             self.clock_domains.cd_phy_pipe_half = ClockDomain()
@@ -353,25 +384,25 @@ class USBSoC(BaseSoC):
                 o_phy_rate=usb_pipe_ctrl.rate,
                 o_phy_elas_buf_mode=usb_pipe_ctrl.elas_buf_mode,
 
-                i_buf_in_addr=0, # TODO
-                i_buf_in_data=0, # TODO
-                i_buf_in_wren=0, # TODO
-                #o_buf_in_request=, # TODO
-                #o_buf_in_ready=, # TODO
-                i_buf_in_commit=0, # TODO
-                i_buf_in_commit_len=0, # TODO
-                #o_buf_in_commit_ack=, # TODO
+                i_buf_in_addr=self.usb3_control.buf_in_addr,
+                i_buf_in_data=self.usb3_control.buf_in_data,
+                i_buf_in_wren=self.usb3_control.buf_in_wren,
+                o_buf_in_request=self.usb3_control.buf_in_request,
+                o_buf_in_ready=self.usb3_control.buf_in_ready,
+                i_buf_in_commit=self.usb3_control.buf_in_commit,
+                i_buf_in_commit_len=self.usb3_control.buf_in_commit_len,
+                o_buf_in_commit_ack=self.usb3_control.buf_in_commit_ack,
 
-                i_buf_out_addr=0, # TODO
-                #o_buf_out_q=, # TODO
-                #o_buf_out_len=, # TODO
-                #o_buf_out_hasdata=, # TODO
-                i_buf_out_arm=0, # TODO
-                #o_buf_out_arm_ack=, # TODO
+                i_buf_out_addr=self.usb3_control.buf_out_addr,
+                o_buf_out_q=self.usb3_control.buf_out_q,
+                o_buf_out_len=self.usb3_control.buf_out_len,
+                o_buf_out_hasdata=self.usb3_control.buf_out_hasdata,
+                i_buf_out_arm=self.usb3_control.buf_out_arm,
+                o_buf_out_arm_ack=self.usb3_control.buf_out_arm_ack,
 
-                #o_vend_req_act=, # TODO
-                #o_vend_req_request=, # TODO
-                #o_vend_req_val=, # TODO
+                #o_vend_req_act=,
+                #o_vend_req_request=,
+                #o_vend_req_val=,
 
                 o_dbg_pipe_state=dbg_pipe_state,
                 o_dbg_ltssm_state=dbg_ltssm_state
